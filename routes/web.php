4100c90aaacd000c1c7b6f96e9ff8a4ed88322f2
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2020-09-17 22:06:45 +0900

    profile

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('top');
+Route::group(['prefix' => 'users', 'middleware' => 'auth'], function () {
+    Route::get('show/{id}', 'UserController@show')->name('users.show');
+    Route::get('edit/{id}', 'UserController@edit')->name('users.edit'); // この行を追記
+    Route::post('update/{id}', 'UserController@update')->name('users.update'); // この行を追記
 });
 
 Auth::routes();
 
+Route::get('/', function () {
+    return view('top');
+    });
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+
