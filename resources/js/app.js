31ebde620c5c4053424ee8346456522d18a2b042
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2020-09-18 17:06:47 +0900

    chat room

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,3 +9,25 @@ require('./jquery.transform2d');
 require('./jTinder');
 
 require('./chat');
+
+$.ajax({
+  type: "GET",
+  url: "request.php",
+  data: { 'データ': data },
+  dataType: "json",
+  scriptCharset: 'utf-8',
+  headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+  }
+})
+  .then(
+      function (param) {
+          //　通信後の処理
+      },
+      function (XMLHttpRequest, textStatus, errorThrown) {
+          //　トークンエラー処理
+          if (XMLHttpRequest.status == '419') {
+              //　リロード
+              location.reload();
+          }
+      });
